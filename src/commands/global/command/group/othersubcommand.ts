4b0a95085e169209990f97b4c38329588a9ebcbd
Nat <wmainvielle@gmail.com>
2021-12-01 11:14:05 +0100

    [stable] exemple for command with sub commands/groups

---
+++ src/commands/global/command/group/othersubcommand.ts
@@ -0,0 +1,22 @@
+import { CommandOption } from 'src/typings';
+
+/**
+ * If you need create other files for command options :
+ * - Create a folder nammed with command body name in the same folder
+ * - Create your sub commands or sub group commands files in this folder
+ * - if you need create other files for group command options :
+ * - Create a folder nammed with group command name in the {commandName} folder
+ * - create your sub commands files in this folder
+ */
+
+export = <CommandOption>{
+  option: {
+    type: 'SUB_COMMAND',
+    name: 'othersubcommand',
+    description: 'subcommand of a command group',
+  },
+
+  exec: async () => {
+    // Code here
+  },
+};
