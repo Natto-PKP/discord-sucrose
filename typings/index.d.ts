85c1be13be7d5839ff7908c40f4789acf6b19cb1
Natto-PKP <wmainvielle@gmail.com>
2022-06-03 12:33:58 +0200

    add Codes to index.d.t

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,17 @@
 /* eslint-disable max-classes-per-file */
 
 import type Discord from 'discord.js';
-import type { Code } from '../src/enums/errors';
+
+export enum Codes {
+  'FATAL' = '\x1b[1m\x1b[31m🔥 FATAL\x1b[0m',
+  'ERROR' = '\x1b[1m\x1b[31m✖ ERROR\x1b[0m',
+  'WARN' = '\x1b[1m\x1b[33m🔔 WARN\x1b[0m',
+  'INFO' = '\x1b[1m\x1b[36m🔎 INFO\x1b[0m',
+  'SUCCESS' = '\x1b[1m\x1b[32m✔ SUCCESS\x1b[0m',
+}
+
+export type Code = keyof typeof Codes;
+export type ErrorCode = 'FATAL' | 'ERROR' | 'WARN';
 
 // ? MANAGERS
 // # command manager
