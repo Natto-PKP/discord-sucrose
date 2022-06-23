d2da437042e931341d62ad89fdef681bcf87f53b
Natto-PKP <wmainvielle@gmail.com>
2022-06-23 13:10:30 +0200

    v4

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,6 @@ import Logger from './services/Logger';
 import Sucrose from './structures/Sucrose';
 
 export {
-  ChatInput,
-  Button,
-  InteractionContent,
-  EventHandler,
-  MessageContextMenu,
-  Permissions,
-  SelectMenu,
-  SubCommand,
-  SubCommandGroup,
-  SucroseOptions,
-  UserContextMenu,
-} from '../typings';
-
-export {
   Logger,
   Sucrose,
 };
